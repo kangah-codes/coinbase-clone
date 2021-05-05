84496a1f47659a1db73259bac54d11c6a1ea8c12
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-05-05 01:37:19 +0000

    chore: Added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,48 @@ module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				coinbase: {
+					DEFAULT: "#2068FF",
+					green: "#05B169",
+				},
+			},
+		},
+		fontFamily: {
+			display: ["Graphik", "system-ui"],
+			sans: [
+				"Graphik",
+				"system-ui",
+				"-apple-system",
+				"BlinkMacSystemFont",
+				'"Segoe UI"',
+				"Roboto",
+				'"Helvetica Neue"',
+				"Arial",
+				'"Noto Sans"',
+				"sans-serif",
+				'"Apple Color Emoji"',
+				'"Segoe UI Emoji"',
+				'"Segoe UI Symbol"',
+				'"Noto Color Emoji"',
+			],
+			serif: [
+				"Georgia",
+				"Cambria",
+				'"Times New Roman"',
+				"Times",
+				"serif",
+			],
+			mono: [
+				"Menlo",
+				"Monaco",
+				"Consolas",
+				'"Liberation Mono"',
+				'"Courier New"',
+				"monospace",
+			],
+		},
 	},
 	variants: {
 		extend: {},
